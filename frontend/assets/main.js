9150b528a65fd28001cab0939fa9e6fa5a2788d1
nataliachojnacka <natalia.chojnacka1099@gmail.com>
2019-12-17 17:45:59 +0100

    geojson

--- frontend/assets/main.js
+++ frontend/assets/main.js
@@ -12,3 +12,16 @@
       zoom: 11
     })
   });
+
+
+  const punkty = {
+    "type": "FeatureCollection",
+    "name": "alepunkty",
+    "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+    "features": [
+    { "type": "Feature", "properties": { "id": 1 }, "geometry": { "type": "Point", "coordinates": [ 18.521865983562616, 52.588523646497229 ] } },
+    { "type": "Feature", "properties": { "id": 2 }, "geometry": { "type": "Point", "coordinates": [ 17.841949595552013, 53.370058103459442 ] } },
+    { "type": "Feature", "properties": { "id": 3 }, "geometry": { "type": "Point", "coordinates": [ 20.115724177973068, 51.768080556753695 ] } },
+    { "type": "Feature", "properties": { "id": 4 }, "geometry": { "type": "Point", "coordinates": [ 18.971454501754682, 51.583520931557182 ] } }
+    ]
+    }
