91cb7aa3428124a92262a05758627b224addefeb
Patryk Jezierski <patryk.jezierski@gis-support.pl>
2020-04-17 01:02:58 +0200

    Dodawanie baz i poprawione wyświetlanie info

--- frontend/assets/main.js
+++ frontend/assets/main.js
@@ -36,7 +36,10 @@ var vectorLayer = new ol.layer.Vector({
   name: "features",
   source: new ol.source.Vector({
     url: "http://3.120.210.65:5000/features",
-    format: new ol.format.GeoJSON(),
+    format: new ol.format.GeoJSON({
+      dataProjection: "EPSG:4326",
+      featureProjection: "EPSG:3857",
+    }),
   }),
   style: style,
 });
@@ -59,6 +62,13 @@ closer.onclick = function () {
   return false;
 };
 
+var source = new ol.source.Vector({ wrapX: false });
+
+var drawLayer = new ol.layer.Vector({
+  source: source,
+  style: style,
+});
+
 var map = new ol.Map({
   layers: [
     new ol.layer.Tile({
@@ -68,6 +78,7 @@ var map = new ol.Map({
       }),
     }),
     vectorLayer,
+    drawLayer,
   ],
   overlays: [overlay],
   target: "map",
@@ -76,6 +87,22 @@ var map = new ol.Map({
     zoom: 6,
   }),
 });
+
+var draw;
+function addInteraction() {
+  draw = new ol.interaction.Draw({
+    source: drawLayer.getSource(),
+    type: "Point",
+    style: style,
+  });
+  draw.on("drawend", () => {
+    draw.setActive(false);
+  });
+  draw.setActive(false);
+  map.addInteraction(draw);
+}
+
+addInteraction();
 
 function getProp(feature) {
   return feature.getProperties();
@@ -115,10 +142,12 @@ function newElem(feature) {
     .appendChild(listEl);
 }
 
-let simpleFeatures;
-map.once("rendercomplete", function (event) {
+function createList() {
   const features = vectorLayer.getSource().getFeatures();
   simpleFeatures = features.map(getProp);
+  document.getElementsByClassName(
+    "list-group-item list-group-item-action list-group-item-secondary"
+  )[0].innerHTML = "";
   simpleFeatures.forEach(newElem);
   document
     .getElementsByClassName(
@@ -127,11 +156,19 @@ map.once("rendercomplete", function (event) {
     .addEventListener("click", (e) => {
       listEventHandler(e);
     });
+}
+let simpleFeatures;
+map.once("rendercomplete", function (event) {
+  createList();
 });
 
 map.on("singleclick", function (evt) {
   var coordinate = evt.coordinate;
-  const properties = map.getFeaturesAtPixel(evt.pixel)[0].getProperties();
+  const featuresAtPixel = map.getFeaturesAtPixel(evt.pixel, {
+    layerFilter: (test) => test.get("name") === "features",
+  });
+  if (!featuresAtPixel[0]) return;
+  const properties = featuresAtPixel[0].getProperties();
   content.innerHTML = `<span class="popup_content_text"> Nazwa: ${properties.name} <br>
                         Adres: ${properties.address} <br> </span>`;
   overlay.setPosition(coordinate);
@@ -140,7 +177,8 @@ map.on("singleclick", function (evt) {
   const activeList = document.getElementsByClassName("active");
   const jsList = Array.from(activeList);
   if (jsList[0]) {
-    jsList[0].classList.remove("active");}
+    jsList[0].classList.remove("active");
+  }
   listEl.classList.add("active");
 });
 
@@ -157,27 +195,71 @@ function hideList() {
   )[0].style.display = "block";
 }
 
-
 function openInfo(id) {
   document.getElementsByClassName("feature__wrapper")[0].style.display =
     "inline";
   document.getElementsByClassName(
     "list-group-item list-group-item-action list-group-item-secondary"
   )[0].style.display = "none";
-  const infoList = document.getElementsByClassName('feature__info')
-  infoList[0].innerHTML = ''
+  const infoList = document.getElementsByClassName("feature__info");
+  infoList[0].innerHTML = "";
   const feature = simpleFeatures.find(function (simpleFeature) {
     return simpleFeature.id == id;
   });
-  for (property in feature) {
-    if (property != "geometry") {
+  const documentFragment = document.createDocumentFragment();
+
+  for (let property in feature) {
+    if (property !== "geometry") {
       const baseElement = document.createElement("li");
-      baseElement.innerHTML = `${dict[property]}: ${feature[property]}`;
-      document
-        .getElementsByClassName("feature__info")[0]
-        .appendChild(baseElement);
+      const baseElementTitle = document.createElement("div");
+      baseElementTitle.innerHTML = `${dict[property]}:`;
+      baseElement.appendChild(baseElementTitle);
+      const baseElementUL = document.createElement("ul");
+      const value = feature[property];
+      if (!value) {
+        const baseElementLI = document.createElement("li");
+        baseElementLI.innerHTML = "brak danych";
+        baseElementUL.appendChild(baseElementLI);
+      } else {
+        String(value)
+          .split(";")
+          .forEach((subVal) => {
+            if (!subVal) return;
+            const baseElementLI = document.createElement("li");
+            baseElementLI.innerHTML = subVal;
+            baseElementUL.appendChild(baseElementLI);
+          });
+      }
+
+      baseElement.appendChild(baseElementUL);
+      documentFragment.appendChild(baseElement);
     }
   }
+  document
+    .getElementsByClassName("feature__info")[0]
+    .appendChild(documentFragment);
+}
+function reloadFeatures() {
+  var req = new XMLHttpRequest();
+  req.open(
+    "GET",
+    "http://3.120.210.65:5000/features",
+    true
+  ); /* Argument trzeci, wartość true, określa, że żądanie ma być asynchroniczne */
+  req.onload = function (aEvt) {
+    if (req.status == 200) {
+      vectorLayer.getSource().clear();
+      const resObject = JSON.parse(req.responseText);
+      const features = new ol.format.GeoJSON({
+        dataProjection: "EPSG:4326",
+        featureProjection: "EPSG:3857",
+      }).readFeatures(resObject);
+      vectorLayer.getSource().addFeatures(features);
+      vectorLayer.setStyle(style);
+      createList();
+    }
+  };
+  req.send(null);
 }
 
 function hideAddBaseList() {
@@ -191,27 +273,112 @@ function showAddBaseList(id) {
     "inline";
   document.getElementsByClassName("features__wrapper")[0].style.display =
     "none";
-  const baseList = document.getElementsByClassName('addBase__form')
-  baseList[0].innerHTML = ''
-  const feature = simpleFeatures[0]
+  const baseList = document.getElementsByClassName("addBase__form");
+  baseList[0].innerHTML = "";
+  const feature = simpleFeatures[0];
+  const addBaseForm = document.getElementsByClassName("addBase__form")[0];
+
+  const addBaseButton = document.createElement("input");
+  addBaseButton.addEventListener("click", () => {
+    draw.setActive(true);
+    addBaseButton.disabled = true;
+  });
+  addBaseButton.type = "button";
+  addBaseButton.value = "Dodaj bazę";
+  addBaseForm.appendChild(addBaseButton);
+
+  const removeBaseButton = document.createElement("input");
+  removeBaseButton.addEventListener("click", () => {
+    drawLayer.getSource().clear();
+    addBaseButton.disabled = false;
+  });
+  removeBaseButton.type = "button";
+  removeBaseButton.value = "Usuń bazę";
+  addBaseForm.appendChild(removeBaseButton);
+
   for (property in feature) {
     if (property != "geometry" && property != "id") {
       const formLabel = document.createElement("label");
       formLabel.innerHTML = `${dict[property]}:`;
-      formLabel.htmlFor = property
-      document
-        .getElementsByClassName("addBase__form")[0]
-        .appendChild(formLabel);
-      const formInput = document.createElement("input")
-      formInput.type = "text"
-      formInput.id = formLabel.htmlFor
-      formInput.name = "property"
-      formInput.className = "form-control"
-      document.getElementsByClassName("addBase__form")[0].appendChild(formInput);
+      formLabel.htmlFor = property;
+
+      addBaseForm.appendChild(formLabel);
+      const formInput = document.createElement("input");
+      formInput.type = "text";
+      formInput.id = formLabel.htmlFor;
+      formInput.name = property;
+      formInput.className = "form-control";
+      addBaseForm.appendChild(formInput);
     }
   }
-  const submitButton = document.createElement("input")
-  submitButton.type = "submit"
-  submitButton.value = "Prześlij"
-  document.getElementsByClassName("addBase__form")[0].appendChild(submitButton)
+
+  const formLabel = document.createElement("label");
+  formLabel.innerHTML = "Kod dostępu";
+  formLabel.htmlFor = "password";
+  addBaseForm.appendChild(formLabel);
+
+  const formInput = document.createElement("input");
+  formInput.type = "text";
+  formInput.id = formLabel.htmlFor;
+  formInput.name = "password";
+  formInput.className = "form-control";
+  addBaseForm.appendChild(formInput);
+
+  const submitButton = document.createElement("input");
+  submitButton.type = "submit";
+  submitButton.value = "Prześlij";
+
+  submitButton.addEventListener("click", (e) => {
+    const formData = new FormData(addBaseForm);
+
+    const features = drawLayer.getSource().getFeatures();
+
+    if (!features[0]) {
+      alert("Podaj współrzędne bazy");
+      e.preventDefault();
+      return;
+    }
+    const coordinates = features[0].getGeometry().getCoordinates();
+    const transformedCoordinates = ol.proj.transform(
+      coordinates,
+      "EPSG:3857",
+      "EPSG:4326"
+    );
+    const geometry = {
+      type: "Point",
+      coordinates: transformedCoordinates,
+    };
+
+    var object = {};
+    formData.forEach(function (value, key) {
+      object[key] = value;
+    });
+
+    var json = JSON.stringify({ ...object, geometry });
+
+    var req = new XMLHttpRequest();
+    req.open("POST", "http://3.120.210.65:5000/features", true);
+    req.onload = function (oEvent) {
+      if (req.status === 200) {
+        drawLayer.getSource().clear();
+        hideAddBaseList();
+        alert("Poprawnie dodano bazę");
+        // window.location.reload();
+        reloadFeatures();
+        // e.preventDefault();
+      } else {
+        drawLayer.getSource().clear();
+        reloadFeatures();
+        hideAddBaseList();
+        alert("Podczas dodawania bazy wystąpił błąd");
+        // oOutput.innerHTML =
+        // "Error " +
+        // oReq.status +
+        // " occurred when trying to upload your file.<br />";
+      }
+    };
+    req.send(json);
+    e.preventDefault();
+  });
+  addBaseForm.appendChild(submitButton);
 }
