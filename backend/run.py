dae3f324ab8fc5da4d53322d434a9d9b83910309
Jakub Skowroński <jakub.skowronski@gis-support.pl>
2020-04-30 14:57:18 +0200

    poprawka

--- backend/run.py
+++ backend/run.py
@@ -1,8 +1,7 @@
 from app import create_app
 
 
-app = create_app('production')
+app = create_app()
 
 if __name__ == '__main__':
-    app = create_app()
     app.run(host=app.config['SERVER_ADDRESS'], port=app.config['SERVER_PORT'])
